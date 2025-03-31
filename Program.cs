e20ac2031c187f6a95c07aaac2d46a13d607bc28
LhGomesss <luishfeitosa01@gmail.com>
2025-03-31 09:05:31 -0300

    Programa finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("-----Forca-----");
+Console.WriteLine("Qual a palavra escolhida?");
+string Palavra = Console.ReadLine()!;
+
+Console.WriteLine("Qual a letra que você escolhe?");
+string Letra = Console.ReadLine()!.Trim().Substring(0,1).ToLower();
+
+bool LetraExiste = Palavra.ToLower().Contains(Letra);
+
+
+Console.WriteLine($"A sua letra está na palavra escolhida? {LetraExiste}");
+
